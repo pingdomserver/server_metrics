88c7ba89bdef5dfed4a3fb0a8b99f41aadab1289
Andre Lewis <andre@highgroove.com>
2013-11-01 14:14:38 -0700

    complete rework of processes

--- lib/server_metrics/collectors/processes.rb
+++ lib/server_metrics/collectors/processes.rb
@@ -1,9 +1,9 @@
 require 'sys/proctable'
+require 'server_metrics/system_info'
 
 # Collects information on processes. Groups processes running under the same command, and sums up their CPU & memory usage.
 # CPU is calculated **since the last run**
 #
-
 class ServerMetrics::Processes
 
   def initialize(options={})
@@ -51,45 +51,57 @@ class ServerMetrics::Processes
   # and calculates CPU time for each process. Since CPU time has to be calculated relative to the last sample,
   # the collector has to be run twice to get CPU data.
   def calculate_processes
-    ## 1. get a list of all processes grouped by command
-    processes = Sys::ProcTable.ps
-    grouped = Hash.new
-    processes.each do |proc|
-      grouped[proc.comm] ||= {
-          :count => 0,
-          :raw_cpu => 0,
-          :cpu => 0,
-          :memory => 0,
-          :uid => 0,
-          :cmdlines => []
-      }
-      grouped[proc.comm][:count] += 1
-      grouped[proc.comm][:raw_cpu] += proc.cutime + proc.cstime
-      grouped[proc.comm][:memory] += proc.rss.to_f / 1024.0
-      grouped[proc.comm][:uid] = proc.uid
-      grouped[proc.comm][:cmdlines] << proc.cmdline if !grouped[proc.comm][:cmdlines].include?(proc.cmdline)
-    end # processes.each
 
-    ## 2. loop through each and calculate the CPU time. To do this, you need to compare the current values against the last run
+    ## 1. get a list of all processes
+    processes = Sys::ProcTable.ps.map{|p| ServerMetrics::Processes::Process.new(p) } # our Process object adds a method and adds some behavior
+
+    ## 2. loop through each process and calculate the CPU time.
+    # The CPU values returned by ProcTable are cumulative for the life of the process, which is not what we want.
+    # So, we rely on @last_process_list to make this calculation. If a process wasn't around last time, we use it's cumulative CPU time so far, which will be accurate enough.
     now = Time.now
-    if @last_run and @last_process_list
+    aggregate_recent_cpu_of_all_processes = 1.0
+    if @last_run && @last_process_list
       elapsed_time = now - @last_run # in seconds
       if elapsed_time >= 1
-        grouped.each do |name, values|
-          # if the process was around last time, calculate CPU relative to the last run. If the process wasn't around last time, return CPU since the process start
-          if last_values = @last_process_list[name]
-            cpu_since_last_sample = values[:raw_cpu] - last_values[:raw_cpu]
-            grouped[name][:cpu] = (cpu_since_last_sample/(elapsed_time * ServerMetrics::SystemInfo.num_processors))*100
+        processes.each do |p|
+          if last_cpu = @last_process_list[p.pid]
+            p.recent_cpu = (p.combined_cpu - last_cpu)/elapsed_time
           else
-            grouped[name][:cpu] = (values[:raw_cpu]/ServerMetrics::SystemInfo.num_processors)*100
+            p.recent_cpu = p.combined_cpu # this process wasn't around last time, so just use the cumulative CPU time so far
           end
+          aggregate_recent_cpu_of_all_processes = aggregate_recent_cpu_of_all_processes + p.recent_cpu
         end
       end
     end
-    @last_process_list = grouped
+
+    ## 3. now hat we have the aggregate CPU usage for all processes, loop through the processes once more and set the recent_cpu_percentage.
+    # note that this value is normalized for the number of processors
+    processes.each {|p| p.recent_cpu_percentage =  (p.recent_cpu / aggregate_recent_cpu_of_all_processes) * 100.0 / ServerMetrics::SystemInfo.num_processors }
+
+    ## 3. group by command and aggregate the CPU
+    grouped = {}
+    processes.each do |proc|
+      grouped[proc.comm] ||= {
+          :cpu => 0,
+          :memory => 0,
+          :count => 0,
+          :cmdlines => []
+      }
+      grouped[proc.comm][:count]    += 1
+      grouped[proc.comm][:cpu]      += proc.recent_cpu_percentage
+      grouped[proc.comm][:memory]   += proc.rss.to_f / 1024.0
+      grouped[proc.comm][:cmdlines] << proc.cmdline if !grouped[proc.comm][:cmdlines].include?(proc.cmdline)
+    end # processes.each
+
+    # {pid => cpu_snapshot, pid2 => cpu_snapshot ...}
+    processes_to_store = processes.inject(Hash.new) do |hash, proc|
+      hash[proc.pid] = proc.combined_cpu
+      hash
+    end
+
+    @last_process_list = processes_to_store
     @last_run = now
 
-    #grouped.select{|k,v| v[:cpu]} # only return processes that have been around for two cycles, so we've been been able to calculate the CPU
     grouped
   end
 
@@ -115,4 +127,22 @@ class ServerMetrics::Processes
     p
   end
 
+  # a thin wrapper around Sys:ProcTable's ProcTableStruct. We're using it to add some fields and behavior.
+  # Beyond what we're adding, it just passes through to its instance of ProcTableStruct
+  class Process
+    attr_accessor :recent_cpu, :recent_cpu_percentage # used to store the calculation of CPU since last sample
+    def initialize(proctable_struct)
+      @pts=proctable_struct
+      @recent_cpu = 0
+    end
+    def combined_cpu
+      # best thread I've seen on cutime vs utime & cstime vs stime: https://www.ruby-forum.com/topic/93176
+      # trying the metric that doesn't include the consumption of child processes
+      utime + stime
+    end
+    # delegate everything else to ProcTable::Struct
+    def method_missing(sym, *args, &block)
+      @pts.send sym, *args, &block
+    end
+  end
 end
