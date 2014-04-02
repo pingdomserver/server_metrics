a1ff0d756899d56d637fb4a715ea2183351f297d
Björn Albers <bjoernalbers@googlemail.com>
2014-04-02 19:42:00 +0200

    Extract method ServerMetrics::Disk#disk_stats
    - Add specs and remove now obsolete specs.
    - Refactor @disk_stats into method with build-in error handling for OS X.
    - Bump version.

--- spec/server_metrics/collectors/disk_spec.rb
+++ spec/server_metrics/collectors/disk_spec.rb
@@ -3,17 +3,25 @@ require 'spec_helper'
 describe ServerMetrics::Disk do
   let(:disk) { ServerMetrics::Disk.new }
 
-  describe '#build_report' do
-    it 'does not parse /proc/diskstats when on OS X' do
-      disk.should_receive(:osx?).and_return(true)
-      File.should_not_receive(:read).with('/proc/diskstats')
-      expect(disk.run).to be
+  describe '#iostat' do
+    it 'uses Disk#diskstats instead @disk_stats' do
+      disk.should_receive(:disk_stats).and_return( [] )
+      expect {
+        disk.send(:iostat, '/dev/chunky')
+      }.to_not raise_error
+    end
+  end
+
+  describe '#disk_stats' do
+    it 'returns lines from /proc/diskstats as array' do
+      File.should_receive(:readlines).with('/proc/diskstats').
+        and_return( %w(chunky bacon) )
+      expect(disk.send(:disk_stats)).to eq %w(chunky bacon)
     end
 
-    it 'parses /proc/diskstats when not on OS X' do
-      disk.should_receive(:osx?).and_return(false)
-      File.should_receive(:read).with('/proc/diskstats').and_return('')
-      expect(disk.run).to be
+    it 'returns empty array when /proc/diskstats is missing' do
+      File.stub(:readlines).and_raise(Errno::ENOENT)
+      expect(disk.send(:disk_stats)).to eq []
     end
   end
 end
